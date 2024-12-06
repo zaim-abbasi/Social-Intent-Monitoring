0cb2d8ff72c9b70c9165c41aaefba0bc46e8bcc7
Zaim Abbasi <zaim.k.abbasi@gmail.com>
2024-12-06 21:21:57 +0500

    Revert "fixed login/signup issues"
    This reverts commit a9c9ed478a055bf2cad420f7aa3756d26da1d1d0.

--- src/server/models/User.js
+++ src/server/models/User.js
@@ -32,7 +32,7 @@ const userSchema = new mongoose.Schema({
   password: {
 
     type: String,
-    required: [true, 'Password is requiredDD'],
+    required: [true, 'Password is required'],
     minlength: [8, 'Password must be at least 8 characters long']
   },
   keywords: [keywordSchema],
